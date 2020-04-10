872e30e4a242f51a5c5ab087bf40f939f188cf54
ncrd <1763000278@qq.com>
2020-04-10 17:17:08 +0800

    修改  rpc相关bug

--- springCloud-parent/springCloud-api/src/main/java/cn/ncu/entity/SqlToEnumEnum.java
+++ springCloud-parent/springCloud-api/src/main/java/cn/ncu/entity/SqlToEnumEnum.java
@@ -5,15 +5,21 @@ import lombok.Getter;
 @Getter
 public enum SqlToEnumEnum {
   
-	SUPER_ADMIN(1),           ORG_ADMIN(2),  ORG_TEACHER(3);
-    
-	private Integer code;
-    
-    private   SqlToEnumEnum(Integer  code) {
-		this.code=code;
-	}
-    
+		SUPER_ADMIN(1),           ORG_ADMIN(2),  ORG_TEACHER(3);
+	
+		private Integer code;
+	
+	private   SqlToEnumEnum(Integer  code) {
+			this.code=code;
+		}
+	
+	
 
+	
+	
+	
+    
+    
 	/*	public static void main(String[] args) {
 		System.out.println(SqlToEnumEnum.ORG_ADMIN.getCode());
 		}*/
